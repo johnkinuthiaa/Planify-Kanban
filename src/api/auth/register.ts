ade9e10a3c06c5f950d412952ee02d5e289a3a18
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-04-10 14:29:21 +0300

    registration page

---
+++ src/api/auth/register.ts
@@ -0,0 +1,33 @@
+type RegisterProps ={
+    username:string,
+    email:string,
+    password:string
+}
+export const RegisterUser = async ({username,email,password}:RegisterProps)=>{
+    const registerUrl ="http://localhost:8080/api/v1/user/register"
+    const myHeaders =new Headers()
+    myHeaders.append("Content-Type","application/stream+json")
+    try{
+        const response =await fetch(registerUrl,{
+            method:"POST",
+            body:JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers:myHeaders
+        })
+        if(response.ok){
+            const data =await response.json()
+            if(data.statusCode !=201){
+                return (`Error creating user: ${data?.message}`)
+            }else{
+                localStorage?.setItem("User",JSON.stringify(data?.user))
+                return ("User created successfully")
+            }
+        }
+    }catch (e) {
+        return `Error>>${e}`
+    }
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+import Register from "@/app/(pages)/(auth)/register/page";
 export default function Home() {
   return (
       <div>
           <div> main page</div>
+          <div className={"flex items-center gap-2 "}>
+              <button
+                  className={"p-4 rounded bg-white text-black"}
+                  onClick={(e)=>{
+                      e.preventDefault()
+                  }}> <a href={"/register"}>Register</a></button>
+              <button>Login</button>
+          </div>
+
+          <Register/>
       </div>
   )
 }
